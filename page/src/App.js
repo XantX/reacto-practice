9fed1133f74ef4a4a8b34f7f4e35cd9000c72878
XantX <hp_xantx@hotmail.com>
2022-06-30 12:34:46 -0500

    Pruebas de snippets

--- page/src/App.js
+++ page/src/App.js
@@ -6,7 +6,6 @@ function App() {
 		<div className="App">
 			<div className="contenedor-principal">
 				<h1>Here is what our alumni say about freeCodeCamp:</h1>
-
 				<Testimonio
 					nombre="Shawn Wang"
 					pais="Singapore"
